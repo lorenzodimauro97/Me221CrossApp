6040156b94f857ec0afd9452c4fccb57ee681cc0
Scippo97 Sensible Productions <lorenzo.dimauro97@gmail.com>
2025-06-11 23:45:18 +0200

    Fix table text visualization

--- Me221CrossApp.UI/Services/ColorHelper.cs
+++ Me221CrossApp.UI/Services/ColorHelper.cs
@@ -4,12 +4,12 @@ public static class ColorHelper
 {
     public static string GetColorForValue(float value, float min, float max)
     {
-        if (min >= max) return "hsl(240, 70%, 50%)";
+        if (min >= max) return "hsl(240, 50%, 60%)";
 
         var normalizedValue = (value - min) / (max - min);
         
         var hue = (1.0f - normalizedValue) * 240;
 
-        return $"hsl({hue:F0}, 70%, 50%)";
+        return $"hsl({hue:F0}, 70%, 60%)";
     }
 }
